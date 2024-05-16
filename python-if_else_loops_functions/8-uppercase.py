111f1925f535451f8514677db554bfaa1f3592e1
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-16 13:28:45 +0000

    created file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             print("{}".format(chr(ord(i) - 32)), end="")
-            print("\n")
+            print()
