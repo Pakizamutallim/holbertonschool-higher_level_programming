295ee55191ed1d9b146822d30300f82504a7cf8e
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-16 12:28:31 +0000

    created file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 10):
         if i != 8 or j != 9:
             print("{:d}{:d}".format(i, j), end=", ")
         else:
-            print("{:d}{:d}".format(i, j), end="")
+            print("{:d}{:d}".format(i, j))
