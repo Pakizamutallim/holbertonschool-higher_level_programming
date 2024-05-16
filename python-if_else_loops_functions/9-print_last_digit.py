fe8798683301790833d8b80cff4b78351f29eb39
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-16 13:59:57 +0000

    created file

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_digit = number - int(number/10) * 10
+    last_digit = abs(number) % 10
     print("{}".format(last_digit), end="")
     return last_digit
