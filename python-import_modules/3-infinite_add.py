04ebc758019a140b7dbf59931da5b8665a84dfb5
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-18 09:54:46 +0000

    created

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,6 +4,5 @@ import sys
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
-
-    result = sum(int(argument) for argument in argv)
-        print(result)
+    total = sum(int(arg) for arg in argv)
+    print(total)
