d7823e6a56f0d07ada5422b01d26726a506136d0
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-24 10:57:53 +0000

    created file for python-data-struct

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    new_string = ""
     for i in my_string:
-        new_string = ""
         if i != "C" or i != "c":
             new_string += i
     return new_string
