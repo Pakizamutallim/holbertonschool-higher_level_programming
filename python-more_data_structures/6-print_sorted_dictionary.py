ec5c9453670ee4261d4e8822bc55b40a123d94fb
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-25 19:14:34 +0000

    created

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 def print_sorted_dictionary(a_dictionary):
     sorted_key = sorted(a_dictionary.keys())
-    for key in sorted_key:
-       print("{}: {}".format(key, a_dictionary[key]))
+    for key, item in sorted_key:
+        print("{}: {}".format(key, item))
