8843977c8a7ca56527b350e91010d79de8541cfd
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-25 22:02:36 +0000

    created

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or roman_string is None):
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
         return (0)
-    roman = {
+
+    roman_dict = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -15,11 +17,13 @@ def roman_to_int(roman_string):
     num = 0
 
     for i in range(len(roman_string)):
-        if roman.get(roman_string[i], 0) == 0:
+        if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
+
         if (i != (len(roman_string) - 1) and
-            roman[roman_string[i]] < roman[roman_string[i]]):
-            num += roman[roman_string[i]] * -1
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
+
         else:
-            num += roman[roman_string[i]]
+            num += roman_dict[roman_string[i]]
     return (num)
