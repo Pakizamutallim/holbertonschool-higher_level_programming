5855fe209d0f2017dd86db65b0919ebb652d845b
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-25 21:32:09 +0000

    created

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,9 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_val = max(a_dictionary.values())
+    max_val = max(a_dictionary.values(), default= None)
     for key, val in a_dictionary:
-        if val is None:
-            return None
         if val == max_val:
             return key
