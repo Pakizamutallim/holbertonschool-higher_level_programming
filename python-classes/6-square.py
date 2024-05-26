d37132d196f65e10fe82895e93b89c790936d9f3
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-26 17:51:03 +0000

    created file for classes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,10 +8,6 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Represent the square."""
 
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.position = position
 
