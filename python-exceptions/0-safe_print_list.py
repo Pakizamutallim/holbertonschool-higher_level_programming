9ea7740262756711a1a4616bde138cc7cc9eac14
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-26 13:34:48 +0000

    created file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     num = 0
-    while num > x:
+    while num < x:
         try:
             print("{}".format(my_list[num]), end="")
             num += 1
