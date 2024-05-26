459adf42117a8f142aa7ea782636964a6337d5d7
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-26 16:27:58 +0000

    created file for classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,8 @@
 
 class Square:
     def __init__(self, size=0):
-        self.__size = size
         if type(size) is not int:
-            print("size must be >= 0")
-            raise ValueError
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
