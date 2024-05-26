3246e978fc50fe284725e2a988428656063f5feb
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-26 11:02:40 +0000

    created file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     num = 0
     try:
         while num > x:
-            print(my_list[num], end="")
+            print("{}".format(my_list[num]), end="")
             num += 1
     except IndexError:
         pass
