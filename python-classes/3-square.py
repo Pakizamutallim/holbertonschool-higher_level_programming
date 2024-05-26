8fd46f24d7d00cf15408e944bd6166fdf5dc8815
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-26 16:36:33 +0000

    created file for classes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,6 +3,9 @@
 
 
 class Square:
+    """Represent a square."""
+
+
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -10,4 +13,4 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
-        return self.area()
+        return (self.__size * self.__size)
