858f7bfc4c8d90bee5d56e558e1ef4f9ee9c61df
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-27 17:02:52 +0000

    created

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,29 +11,36 @@ def matrix_divided(matrix, div):
     Divides all elements of a matrix by a given divisor.
     
     Parameters:
-    matrix (list of lists): A matrix to be divided, must be a list of lists of integers or floats.
+    matrix (list of lists): A matrix to be divided, must be a list of lists
+    of integers or floats.
     div (int or float): The divisor, must be a non-zero number.
     
     Returns:
-    list of lists: A new matrix with all elements divided by the divisor, rounded to 2 decimal places.
+    list of lists: A new matrix with all elements divided by the divisor,
+    rounded to 2 decimal places.
     
     Raises:
-    TypeError: If the matrix is not a list of lists of integers or floats, or if div is not a number.
+    TypeError: If the matrix is not a list of lists of integers or floats,
+    or if div is not a number.
     TypeError: If each row of the matrix does not have the same size.
     ZeroDivisionError: If div is equal to 0.
     """
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not 
+            all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)
+                of integers/floats")
     for row in matrix:
         if not all(isinstance(el, (int, float)) for el in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists)
+                    of integers/floats")
 
     if len(matrix) > 0:
         row_length = len(matrix[0])
         for row in matrix:
             if len(row) != row_length:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix must have
+                        the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
