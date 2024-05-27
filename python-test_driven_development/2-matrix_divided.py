4b52cc717e7bd792569621b95bb97307c49fe6a6
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-27 16:58:13 +0000

    created

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,27 +25,22 @@ def matrix_divided(matrix, div):
 
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not matrix or not matrix[0]:
-        raise ValueError("matrix cannot be empty")
-
     for row in matrix:
         if not all(isinstance(el, (int, float)) for el in row):
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    row_length = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")
-
+    if len(matrix) > 0:
+        row_length = len(matrix[0])
+        for row in matrix:
+            if len(row) != row_length:
+                raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
     new_matrix = []
     for row in matrix:
         new_row = [round(el / div, 2) for el in row]
         new_matrix.append(new_row)
-
 
     return new_matrix
