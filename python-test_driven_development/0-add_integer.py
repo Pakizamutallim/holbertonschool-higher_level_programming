9a32b5c8a9376c3da18362e5324af13f36f21f0a
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-27 15:50:08 +0000

    created

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,7 +17,20 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
+
+    if a == float('inf') or a == float('-inf'):
+        raise OverflowError("a is too large to convert to an integer")
+    if b == float('inf') or b == float('-inf'):
+        raise OverflowError("b is too large to convert to an integer")
+
+    try:
+        a = int(a)
+    except OverflowError:
+        raise OverflowError("a is too large to convert to an integer")
+
+    try:
+        b = int(b)
+    except OverflowError:
+        raise OverflowError("b is too large to convert to an integer")
 
     return (a + b)
