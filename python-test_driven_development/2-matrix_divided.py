1600aa98e35dc3f00866cfa99fda6c2961d92419
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-28 14:42:46 +0000

    created

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -26,23 +26,20 @@ def matrix_divided(matrix, div):
     ZeroDivisionError: If div is equal to 0.
     """
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(element, int) or isinstance(element, float))
+                    for element in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-    if any(not isinstance(el, (int, float)) for row in matrix for el in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
-        return []
-
-    row_length = len(matrix[0])
-    if not all(len(row) == row_length for row in matrix):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, (int, float)):
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return [[round(el / div, 2) for el in row] for row in matrix]
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
