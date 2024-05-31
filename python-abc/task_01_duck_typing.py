05b420377c9da5994a9e873b8f7a56941890a1ba
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-31 12:55:40 +0000

    crt

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -24,8 +24,6 @@ class Circle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        if width < 0 or height < 0:
-            raise ValueError("Width and height cannot be negative")
         self.width = width
         self.height = height
 
