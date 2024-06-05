36091181c55f9bd0046f8b88c07b6e44b46bda03
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-05 12:03:12 +0000

    created

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+appends a string at the end of a text
+"""
 
 
 def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text
+    """
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
