52d7e088188d22a9f72ef999273f84097c2b0ae4
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-05 13:41:02 +0000

    created

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 save_to_json_file writes an Object to a text file
 """
@@ -10,4 +11,4 @@ def save_to_json_file(my_obj, filename):
     save_to_json_file
     """
     with open(filename, 'w', encoding='utf-8') as f:
-        return(json.dump(my_obj, f)
+        return(json.dump(my_obj, f))
