3027a6d2761156fb02ec80f0607d9cb68096eba8
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-05 12:10:34 +0000

    created

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function returns the JSON representation
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function returns the JSON representation
+    """
+    return json.dumps(my_obj)
