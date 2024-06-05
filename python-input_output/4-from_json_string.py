596d124ecb4084e7416f77eacd2e40a64610ac45
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-05 13:25:53 +0000

    created

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    represented by a JSON string
+    """
+    return json.loads(mu_str)
