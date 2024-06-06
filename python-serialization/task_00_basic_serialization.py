15c764580af81e5f58ceb757e8c84feb05f95de8
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-06 12:47:43 +0000

    created

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,21 +1,26 @@
 import json
-"""
-module that adds the functionality to serialize
-"""
-
 
 def serialize_and_save_to_file(data, filename):
     """
-    module that adds the functionality to serialize
-    """
+    Serialize a Python dictionary and save it to a JSON file.
 
-    with open(filename, 'r', encoding='utf-8') as f:
-        json.dump(data, f)
+    Args:
+        data (dict): The Python dictionary to serialize.
+        filename (str): The name of the output JSON file.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file)
 
 def load_and_deserialize(filename):
     """
-    module that adds the functionality to serialize
-    """
+    Load data from a JSON file and deserialize it into a Python dictionary.
 
+    Args:
+        filename (str): The name of the input JSON file.
+
+    Returns:
+        dict: The deserialized Python dictionary.
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
+
