91ce42ef01169317f55958b0c6325c75bb4afccb
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-06 11:04:34 +0000

    created

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+adds all arguments to a Python list
+and then save them
+"""
+
 import sys
 import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
