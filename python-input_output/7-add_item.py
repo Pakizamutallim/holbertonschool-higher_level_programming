58d13a3a5a138a1250afe90136eabc511402e0ae
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-06 10:55:46 +0000

    created

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+adds all arguments
+"""
+import sys
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+add_file = "add_item.json"
+if path.exists(add_file):
+    json_file = load_from_json_file('add_file')
+else:
+    json_file = []
+
+for i in range(1, len(argv)):
+    json_file.append(argv[1])
+
+save_to_json_file(json_file, add_file)
