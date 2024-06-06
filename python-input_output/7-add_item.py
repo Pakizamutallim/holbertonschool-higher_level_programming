e5745b7466497559d7e819fea2d12cc263e6be4a
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-06 11:05:33 +0000

    created

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""
-adds all arguments to a Python list
-and then save them
-"""
-
-import sys
-import os
+from os import path
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-add_file = "add_item.json"
-if path.exists(add_file):
-    json_file = load_from_json_file('add_file')
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
 else:
-    json_file = []
+    obj_json_file = []
 
 for i in range(1, len(argv)):
-    json_file.append(argv[1])
+    obj_json_file.append(argv[i])
 
-save_to_json_file(json_file, add_file)
+save_to_json_file(obj_json_file, 'add_item.json')
