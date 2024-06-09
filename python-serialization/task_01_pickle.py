fe051e4597c4fd1b97f0d7ebe0681a635ad53229
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-09 08:52:41 +0000

    created

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,57 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+The ''1. Pickling Custom Classes'' module
+"""
+
 import pickle
-"""
-Serialize a Python dictionary to an XML file.
-"""
 
 
 class CustomObject:
-    """
-    Serialize a Python dictionary to an XML file.
-    """
-
     def __init__(self, name, age, is_student):
-        """Initialize a new CustomObject.
-
-        Args:
-            name (str): The name of the person.
-            age (int): The age of the person.
-            is_student (bool): Whether the person is a student.
-        """
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """Display the attributes of the CustomObject."""
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Is Student: {self.is_student}")
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Is Student:", self.is_student)
 
     def serialize(self, filename):
-        """Serialize the object to a file using pickle.
-
-        Args:
-            filename (str): The name of the file to write the object to.
-        """
         try:
             with open(filename, 'wb') as file:
                 pickle.dump(self, file)
         except Exception as e:
-            print(f"An error occurred while serializing: {e}")
+            print("Error occurred while serializing:", e)
 
     @classmethod
     def deserialize(cls, filename):
-        """Deserialize an object from a file using pickle.
-
-        Args:
-            filename (str): The name of the file to read the object from.
-
-        Returns:
-            CustomObject: The deserialized object, or None if deserialization fails.
-        """
         try:
             with open(filename, 'rb') as file:
                 return pickle.load(file)
-        except (FileNotFoundError, pickle.PickleError) as e:
-            print(f"An error occurred while deserializing: {e}")
+        except FileNotFoundError:
+            print("File not found.")
+        except Exception as e:
+            print("Error occurred while deserializing:", e)
             return None
