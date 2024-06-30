5159e4f6dbb0a0598287e7046a5b628c37aafd4d
Pakizamutallim <62bq5tn@code.edu.az>
2024-06-30 22:05:33 +0400

    created files

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-# <mysql password> \
-# <database name>
-import sys
-import MySQLdb
-import sqlalchemy
+"""Module for Selecting states starting with N"""
 
-def filter_states(username, password, database):
-    # Connect to MySQL database
-    try:
-        db = MySQLdb.connect(host="localhost",
-                             user=username,
-                             passwd=password,
-                             db=database,
-                             port=3306)
-        cursor = db.cursor()
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
-        # Execute the query to select states starting with 'N'
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    db = MySQLdb.connect(
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
 
-        # Fetch all rows
-        rows = cursor.fetchall()
+    cursor.execute('SELECT * FROM states ORDER BY id')
 
-        # Print each row as per the example format
-        for row in rows:
-            print(row)
+    for state in cursor.fetchall():
+        if state[1][0] == 'N':
+            print(state)
 
-        # Close cursor and connection
+    if cursor:
         cursor.close()
+    if db:
         db.close()
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    filter_states(username, password, database)
